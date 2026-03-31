9948400f8b58a818847a7624f859425cd2221c64
23abutalha <cadettalhasaleem@gmail.co>
2026-03-31 14:05:47 +0500

    Fix dashboard scaling for Vivo Y100

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="https://cdn-icons-png.flaticon.com/512/2622/2622713.png"></link>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>      
       </head>
       <body style={{ margin: 0, background: '#0f172a' }}>{children}</body>
     </html>
